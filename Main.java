70d7a70741686c3e4c4b5d35595fd19bfe548eb2
Marshall Munoz <marshmun@gmail.com>
2017-07-24 07:41:09 -0500

    finished

--- Main.java
+++ Main.java
@@ -1,3 +1,8 @@
+package com.theironyard;
+
+
+import java.util.Arrays;
+
 /**
  * Test Driven Development (TDD)
  * Write your tests first and watch them fail.
@@ -6,26 +11,86 @@
 public class Main {
     public static void main(String[] args) {
         ReversePolishCalc rpc = new ReversePolishCalc();
+        testRPC(rpc);
 
+        Bubble bubble = new Bubble();
+        testBubbleInt(bubble);
+        testBubbleString(bubble);
+    }
+
+    private static void testRPC(ReversePolishCalc rpc) {
+        // Simple test for +
         String rpnExpression = "2.5,4.8,+";
         double expectedResult = 7.3;
         double actualResult = rpc.calculate(rpnExpression);
-        checkResult(rpnExpression, expectedResult, actualResult);
+        checkRPCResult(rpnExpression, expectedResult, actualResult);
+        // Simple test for -
+        rpnExpression = "18.1,4.8,-";
+        expectedResult = 13.3;
+        actualResult = rpc.calculate(rpnExpression);
+        checkRPCResult(rpnExpression, expectedResult, actualResult);
+        // Simple test for *
+        rpnExpression = "3.7,5.0,*";
+        expectedResult = 18.5;
+        actualResult = rpc.calculate(rpnExpression);
+        checkRPCResult(rpnExpression, expectedResult, actualResult);
+        // Simple test for /
+        rpnExpression = "21.0,0.75,/";
+        expectedResult = 28.0;
+        actualResult = rpc.calculate(rpnExpression);
+        checkRPCResult(rpnExpression, expectedResult, actualResult);
 
-        // Write tests for the other operators (-, *, /)
-
-        // Uncomment this test which has many operators
-//        rpnExpression = "5,1,2,+,4,*,+,3,-";
-//        expectedResult = 14.0;
-//        actualResult = rpn.calculate(rpnExpression);
-//        checkResult(rpnExpression, expectedResult, actualResult);
+        // Test which has many operators
+        rpnExpression = "5,1,2,+,4,*,+,3,-";
+        expectedResult = 14.0;
+        actualResult = rpc.calculate(rpnExpression);
+        checkRPCResult(rpnExpression, expectedResult, actualResult);
     }
 
-    private static void checkResult(String expression, double expected, double actual) {
+    private static void checkRPCResult(String expression, double expected, double actual) {
         if (expected == actual) {
             System.out.println("SUCCESS: " + expression + " is " + expected);
         } else {
             System.out.println("ERROR: " + expression + " expected " + expected + " actual " + actual);
+        }
+    }
+
+    private static void testBubbleInt(Bubble bubble) {
+        // Test proper sort of int array
+        int[] unsortedInts = {7,6,3,5};
+        int[] expectedResult = {3,5,6,7};
+        int[] actualResult = bubble.sort(unsortedInts);
+        checkBubbleIntResult(unsortedInts, expectedResult, actualResult);
+    }
+
+    private static void testBubbleString(Bubble bubble) {
+        String[] unsortedStrings = {"A","B","F","D"};
+        String[] expectedResult = {"A","B","D","F"};
+        String[] actualResult = bubble.sort(unsortedStrings);
+        checkBubbleStringResult(unsortedStrings, expectedResult, actualResult);
+
+        unsortedStrings = new String[]{"Zebra", "Giraffe", "Octopus", "Monkey", "Whale", "Bear"};
+        expectedResult = new String[]{"Bear", "Giraffe", "Monkey", "Octopus", "Whale", "Zebra"};
+        actualResult = bubble.sort(unsortedStrings);
+        checkBubbleStringResult(unsortedStrings, expectedResult, actualResult);
+    }
+
+    private static void checkBubbleStringResult(String[] unsorted, String[] expected, String[] actual) {
+        if (Arrays.equals(expected, actual)) {
+            System.out.println("SUCCESS: " + Arrays.deepToString(unsorted) + " sorts to " +
+                    Arrays.deepToString(expected));
+        } else {
+            System.out.println("ERROR: " + Arrays.deepToString(unsorted) + " expected " + Arrays.deepToString(expected)
+                    + " -- actual " + Arrays.deepToString(actual));
+        }
+    }
+
+    private static void checkBubbleIntResult(int[] unsorted, int[] expected, int[] actual) {
+        if (Arrays.equals(expected, actual)) {
+            System.out.println("SUCCESS: " + Arrays.toString(unsorted) + " sorts to " + Arrays.toString(expected));
+        } else {
+            System.out.println("ERROR: " + Arrays.toString(unsorted) + " expected " + Arrays.toString(expected) +
+                    " -- actual " + Arrays.toString(actual));
         }
     }
 }
--- ReversePolishCalc.java
+++ ReversePolishCalc.java
@@ -1,25 +1,44 @@
+package com.theironyard;
+
 
 public class ReversePolishCalc {
 
-    // You'll need a variable here to keep track of the top of the stack
+    private int topOfStack = -1;
 
-    // The array of the input string split up
     private String[] tokens;
 
     // The stack
     private String[] stack;
 
+//    private String[] OPERATORS = new String[]{"+", "-", "*", "/"};
+
     public double calculate(String input) {
 
         // 1. Use the String split method to split the string into tokens at the commas
-        tokens = ...
+        tokens = input.split(",");
 
         // 2. Allocate a stack as big as the number of tokens
-        stack = ...
+        stack =  new String[tokens.length];
 
         // 3. write the algorithm
-        for(int i = 0; i < tokens.length; ++i) {
+        for (int i = 0; i < tokens.length; i++) {
             // calls to push() and pop() and do the math here
+            String token = tokens[i];
+            if (token.equals("+") || token.equals("-") || token.equals("*") || token.equals("/")) {
+                double secondOperand = pop();
+                double firstOperand = pop();
+                if (token.equals("+")) {
+                    push(firstOperand + secondOperand);
+                } else if (token.equals("-")) {
+                    push(firstOperand - secondOperand);
+                } else if (token.equals("*")) {
+                    push(firstOperand * secondOperand);
+                } else if (token.equals("/")) {
+                    push(firstOperand / secondOperand);
+                }
+            } else {
+                push(token);
+            }
         }
 
         // 4. return the result
@@ -27,14 +46,21 @@ public class ReversePolishCalc {
     }
 
     private void push(String number) {
-        // push on the stack
+        topOfStack++;
+        stack[topOfStack] = number;
     }
 
     private void push(double d) {
-        // change the double to a string and then push it on the stack
+        String pushString = String.valueOf(d);
+        push(pushString);
     }
 
     private double pop() {
-        // remove the string from the top of the stack and convert it to a double and return it
+        double popString = Double.parseDouble(stack[topOfStack]);
+        stack[topOfStack] = null;
+        if (topOfStack != 0) {
+            topOfStack--;
+        }
+        return popString;
     }
 }
